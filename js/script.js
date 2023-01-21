bbd22609f516e5bdabd7df2f2dbeee6dae402a7c
Jameslabrie <jameslabrie@bk.ru>
2023-01-21 22:37:49 +0400

    Create: Timer

--- js/script.js
+++ js/script.js
@@ -35,4 +35,43 @@ window.addEventListener("DOMContentLoaded", function () {
   });
 
   // Timer
+
+  let deadline = "2023-01-22";
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date());
+
+    let seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor(t / 1000 / 60 / 60);
+
+    return {
+      total: t,
+      seconds: seconds,
+      minutes: minutes,
+      hours: hours,
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector(".hours"),
+      minutes = timer.querySelector(".minutes"),
+      seconds = timer.querySelector(".seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock("timer", deadline);
 });
